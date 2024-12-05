79a26cca9597dc6f882467ec415c9c6d04b648e7
Andreas Lukita <andreas030503@gmail.com>
2024-12-05 13:08:33 +0800

    add card component

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import Card from "./Card";
 
-interface ResultsProps {
+export interface ResultsProps {
   adult: boolean;
   backdrop_path: string;
   genre_ids: number[];
@@ -23,11 +24,9 @@ interface ResultsComponentProps {
 
 const Results = ({ results }: ResultsComponentProps) => {
   return (
-    <div>
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
       {results.map((result) => (
-        <div key={result.id}>
-          <h2>{result.original_title}</h2>
-        </div>
+        <Card key={result.id} result={result} />
       ))}
     </div>
   );
