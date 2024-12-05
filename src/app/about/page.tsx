567d820fc3eaac3c0a046d52e1603f80d30831ea
Andreas Lukita <andreas030503@gmail.com>
2024-12-05 10:11:22 +0800

    create the about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="max-w-6xl max-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste
+        doloremque temporibus sequi eaque repudiandae neque corrupti sint rerum,
+        suscipit aliquid inventore distinctio eius deleniti perferendis mollitia
+        culpa hic magni vel. Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit. Iste doloremque temporibus sequi eaque repudiandae neque corrupti
+        sint rerum, suscipit aliquid inventore distinctio eius deleniti
+        perferendis mollitia culpa hic magni vel.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste
+        doloremque temporibus sequi eaque repudiandae neque corrupti sint rerum,
+        suscipit aliquid inventore distinctio eius deleniti perferendis mollitia
+        culpa hic magni vel. Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit. Iste doloremque temporibus sequi eaque repudiandae neque corrupti
+        sint rerum, suscipit aliquid inventore distinctio eius deleniti
+        perferendis mollitia culpa hic magni vel.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste
+        doloremque temporibus sequi eaque repudiandae neque corrupti sint rerum,
+        suscipit aliquid inventore distinctio eius deleniti perferendis mollitia
+        culpa hic magni vel. Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit. Iste doloremque temporibus sequi eaque repudiandae neque corrupti
+        sint rerum, suscipit aliquid inventore distinctio eius deleniti
+        perferendis mollitia culpa hic magni vel.
+      </p>
+    </div>
+  );
+};
+
+export default About;
